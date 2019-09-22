ab333599916806d82140a620591fe451a67089cb
Pradeeppop <55526070+Pradeeppop@users.noreply.github.com>
2019-09-23 01:28:54 +0530

    Update q3.js

--- q3.js
+++ q3.js
@@ -3,15 +3,15 @@
 //With the destructuring syntax, you can extract smaller fragments from arrays and objects.
 // Destructuring syntax can be used for variable declaration or variable assignment.
 const student = {
-    firstname: 'Glad',
-    lastname: 'Chinda',
-    country: 'Nigeria'
+    firstname: 'John',
+    lastname: 'Pop',
+    country: 'India'
 };
 
 // Object Destructuring
 const { firstname, lastname, country } = student;
 
-console.log(firstname, lastname, country); // Glad Chinda Nigeri
+console.log(firstname, lastname, country); 
 
 //Here we used object destructuring syntax to assign values to three variables: 
 //firstname, lastname and country using the values from their corresponding keys on the student object.
